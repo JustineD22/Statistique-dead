2954edb9f86f6812e78378692f1f09a1ba8a3d8d
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-15 15:48:22 +0100

    presque bon

--- index.php
+++ index.php
@@ -8,41 +8,30 @@ spl_autoload_register(function ($className) {
 
 $fichier = file('./files/valeurs_mensuelles.csv');
 $filename = "./files/valeurs_mensuelles.csv";
-$handle = fopen($filename, "r");
+$handle = fopen($filename, "rb");
 
-$test = new File("./files/valeurs_mensuelles.csv", "r");
+    $html = "<table>";
+
+while(!feof($handle)) {
+    $ligne = fgets($handle);
+    echo "<br>";
+    //var_dump($ligne);
 
 
+    
+    $fichier = explode(";", $ligne);
+    //var_dump($fichier);
+    for ($i = 0; $i < 1; $i++){ 
+        $html .= "<tr>";
+        $html .= "<td>" . $fichier [0] ."</td>";
+        $html .= "<td>" . $fichier [1] ."</td>";
+        $html .= "<td>" . $fichier [2] ."</td>";
+        $html .= "</tr>";
+    }
+   
 
-$html = "<table>";
-$html .= "<tr>";
-$html .= "<th>Dates</th>";
-$html .= "<th>Nombre de décés</th>";
-$html .= "<th>Lettre</th>";
-$html .= "</tr>";
-
-for ($i = 4; $i < count($fichier); $i++){
-    $html .= "<tr>";
-    $html .= "<td>" . 
-        $fichier[$i][1].$fichier[$i][2].$fichier[$i][3].$fichier[$i][4].$fichier[$i][5].$fichier[$i][6].$fichier[$i][7]
-        ."</td>";
-    $html .= "<td>" . 
-    $fichier[$i][11].$fichier[$i][12].$fichier[$i][13].$fichier[$i][14].$fichier[$i][15]
-    ."</td>";
-    $html .= "<td>" . $fichier[$i][19]. "</td>";
-    $html .= "</tr>";
-}
+    
+} 
 $html .= "</table>";
-
-echo ($html);
-
-// $stat = "<blockquote>";
-
-// for ($i = 4 ; $i < count($fichier) ; $i++) {
-//     $stat .=$fichier[$i];
-//     $stat .= "<br/>";
-// }
-
-// $stat .= "</blockquote>";
-
-// echo $stat;
+echo $html;
+fclose($handle);
