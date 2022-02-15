d6be6c24da3ceadaff5e716a7d331bb5179ba416
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-15 10:30:57 +0100

    file.php

---
+++ classes/File.php
@@ -0,0 +1,48 @@
+<?php
+
+class File {
+    private string $file;
+    private string $method;
+    private int $size = 0;
+    private $handle;
+    private array $arrayMethod = array("r" , "r+" , "w", "w+", "a", "a+", "x", "x+", "c", "c+", "e");
+
+    function __construct(string $file, string $method)
+    {
+        $this->file =$file;
+        if(in_array($method, $this->arrayMethod)) {
+            $this->method = $method;
+            if ($this->handle = fopen($this->file, $this->method)) {
+            $this->size = filesize($this->file); 
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    function lecture(int $sizeParam = null)
+    {
+        if ($sizeParam !== null) {
+            $sizeParam = abs($sizeParam);
+        }
+
+        if($sizeParam > 0 && $sizeParam <= $this->size) {
+            return fread($this->handle, $sizeParam); 
+        }
+        elseif ($sizeParam === null || $sizeParam > $this->size || $sizeParam === 0) {
+            return fread($this->handle, $this->size);
+        }
+        else {
+            return false;
+        }
+    }
+
+    function __destruct()
+    {
+        fclose($this->handle);
+    }
+}
